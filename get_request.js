024d091f01120bde564ea568bbd2ae13da1d83f4
NicholasthedevJacobs <jjacobs.nicholas@gmail.com>
2021-01-09 14:39:13 -0600

    Created a method to GET all of the data from the recipes JSON, and store it in a variable/function.  Will do for specials as well.

--- get_request.js
+++ get_request.js
@@ -1,12 +1,15 @@
+
+
 const axios = require('axios');
 
-axios.get('http://localhost:3001/recipes')
-    .then(resp => {
-        data = resp.data;
-        data.forEach(e => {
-            console.log(`${e.title}, ${e.description}, ${e.servings}`);
-        });
-    })
-    .catch(error => {
-        console.log(error);
-    });    
+
+
+async function makeGetRequest() {
+
+    let res = await axios.get('http://localhost:3001/recipes');
+  
+    let data = res.data;//data should be stored in this variable
+    console.log(data);
+  }
+  
+  makeGetRequest();
